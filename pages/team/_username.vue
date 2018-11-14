0aab6eef575d2f53e2d9e15d45b0c8f275a77f11
Alex <alex@orion.sh>
2018-11-14 11:14:34 -0500

    oups

--- pages/team/_username.vue
+++ pages/team/_username.vue
@@ -13,5 +13,19 @@ export default {
         { hid: 'description', name: 'description', content: `Discover our team member ${this.$route.params.username}` }
       ]
     }
-  }}
+  },
+  async asyncData({ app }) {
+    let team = await app.$axios.$get('/api/team.json')
+    return { team }
+  }
+  // async asyncData() {
+    // var xhr = new XMLHttpRequest()
+    // var self = this
+    // xhr.open('GET', 'http://localhost:3000/api/team.json')
+    // xhr.onload = function () {
+    //   console.log(JSON.parse(xhr.responseText))
+    // }
+    // xhr.send()
+  // }
+}
 </script>
